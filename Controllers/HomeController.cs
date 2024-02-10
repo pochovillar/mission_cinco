11c5240fb55677f0ae05cb4985f3d7be1b56c746
pochovillar <m.villar.barrios@gmail.com>
2024-02-09 21:57:27 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blahsec2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult calc()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/script.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#calculateBtn").click(function () {
+        var hoursInput = parseFloat($("#hoursInput").val());
+        var hourlyRate = parseFloat($("#hourlyRate").val().replace("$", ""));
+        var totalOutput = $("#totalOutput");
+
+        // Validate input
+        if (!isNaN(hoursInput) && hoursInput >= 0) {
+            var total = hoursInput * hourlyRate;
+            totalOutput.text("$" + total.toFixed(2));
+        } else {
+            totalOutput.text("Invalid input. Please enter a positive number of hours.");
+        }
+    });
+});
